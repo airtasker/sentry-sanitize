2252c8287baf896b3610d692f87700d554db5078
Niko Roberts <niko@tasboa.com>
2022-04-11 20:55:13 +1000

    added specs

--- spec/sentry/sanitize_spec.rb
+++ spec/sentry/sanitize_spec.rb
@@ -1,9 +1,33 @@
-RSpec.describe Sentry::Sanitize do
-  it "has a version number" do
-    expect(Sentry::Sanitize::VERSION).not_to be nil
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'cgi'
+require 'uri'
+
+RSpec.describe Sentry::Sanitize::Processor::CustomSanitizeData do
+  let(:sanitize_fields) { ['password', '(?i-mx:.*auth-token$)'] }
+  let(:processor) { described_class.new(sanitize_fields) }
+  let(:filtered) { '********' }
+
+  it 'filters auth-token from headers' do
+    expect(processor.process({ 'headers' => { 'x-auth-token' => 'foobar' } }))
+      .to                 eq({ 'headers' => { 'x-auth-token' => filtered } })
+    expect(processor.process({ 'headers' => { 'X-AUTH-TOKEN' => 'foobar' } }))
+      .to                 eq({ 'headers' => { 'X-AUTH-TOKEN' => filtered } })
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'filters password from HTML form request body' do
+    expect(processor.process({ 'data' => { 'password' => '123', 'account' => { 'password' => '123' } } }))
+      .to                 eq({ 'data' => { 'password' => filtered, 'account' => { 'password' => filtered } } })
+  end
+
+  it 'filters password from JSON request body' do
+    expect(processor.process({ 'data' => JSON.dump({ 'password' => '123', 'account' => { 'password' => '123' } }) }))
+      .to                 eq({ 'data' => JSON.dump({ 'password' => filtered, 'account' => { 'password' => filtered } }) })
+  end
+
+  it 'filters password from query parameters' do
+    expect(processor.process({ 'request' => { 'query_string' => 'foo=bar&password=test' } }))
+      .to                 eq({ 'request' => { 'query_string' => 'foo=bar&password=********' } })
   end
 end
